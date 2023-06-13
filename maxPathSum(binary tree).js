c7a0ae495747258c072c166c18636d1a13083b8c
QuartzEther <quartz.ether@gmail.com>
2023-06-13 23:35:14 +0300

    Update maxPathSum(binary tree).js

--- maxPathSum(binary tree).js
+++ maxPathSum(binary tree).js
@@ -1,27 +1,27 @@
-
 //------------My solution------------
-// function maxPath(root){
-//     let max = -Infinity;
+
+function maxPath(root){
+    let max = -Infinity;
     
-//     const maxGain = (root) => {
-//         if(!root) return 0;
+    const maxGain = (root) => {
+        if(!root) return 0;
     
-//         let tmpL = maxGain(root.left), tmpR = maxGain(root.right);
-//         if (tmpL < 0) tmpL = 0;
-//         if (tmpR < 0) tmpR = 0;
+        let tmpL = maxGain(root.left), tmpR = maxGain(root.right);
+        if (tmpL < 0) tmpL = 0;
+        if (tmpR < 0) tmpR = 0;
         
-//         let fullNode = root.val+tmpL+tmpR;
+        let fullNode = root.val+tmpL+tmpR;
         
-//         let tmpMax = fullNode > root.val+tmpL && fullNode > root.val+tmpR ? fullNode : tmpL>tmpR ? root.val+tmpL : root.val+tmpR;
+        let tmpMax = fullNode > root.val+tmpL && fullNode > root.val+tmpR ? fullNode : tmpL>tmpR ? root.val+tmpL : root.val+tmpR;
         
-//         if (max<tmpMax) max = tmpMax;
+        if (max<tmpMax) max = tmpMax;
         
-//         return tmpL>tmpR ? root.val+tmpL : root.val+tmpR;
-//     }
+        return tmpL>tmpR ? root.val+tmpL : root.val+tmpR;
+    }
     
-//     maxGain(root);
-//     return max;
-// }
+    maxGain(root);
+    return max;
+}
 //-----------------------------------
 // Путь в бинарном дереве — это последовательность узлов, в которой каждая
 // пара соседних узлов в последовательности имеет соединяющее их ребро.
@@ -32,23 +32,26 @@
 
 // Сумма максимального пути двоичного дерева
 
-function maxPathSum(root) { 
-    let maxSum = Number.NEGATIVE_INFINITY; 
+
+//same solution with Math.max() and more beauties
+
+// function maxPathSum(root) { 
+//     let maxSum = Number.NEGATIVE_INFINITY; 
  
-    function maxGain(node) { 
-        if (node === null) { 
-            return 0; 
-        } 
+//     function maxGain(node) { 
+//         if (node === null) { 
+//             return 0; 
+//         } 
          
-        const leftGain = Math.max(maxGain(node.left), 0); 
-        const rightGain = Math.max(maxGain(node.right), 0); 
+//         const leftGain = Math.max(maxGain(node.left), 0); 
+//         const rightGain = Math.max(maxGain(node.right), 0); 
  
-        const value = node.val + leftGain + rightGain; 
-        maxSum = Math.max(maxSum, value); 
+//         const value = node.val + leftGain + rightGain; 
+//         maxSum = Math.max(maxSum, value); 
          
-        return node.val + Math.max(leftGain, rightGain); 
-    } 
+//         return node.val + Math.max(leftGain, rightGain); 
+//     } 
  
-    maxGain(root); 
-    return maxSum; 
-}
+//     maxGain(root); 
+//     return maxSum; 
+// }
